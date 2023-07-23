54e8d31e4819d4fed136e25df6e1aa87def8fca1
Ruben te Wierik <rtewierik64@gmail.com>
2023-07-23 10:10:57 +0200

    Implement validation correctly.

--- DropboxLike.Api/Program.cs
+++ DropboxLike.Api/Program.cs
@@ -1,5 +1,5 @@
+using System.Security.Claims;
 using System.Text;
-using DropboxLike.Api.Authentication;
 using DropboxLike.Domain.Configuration;
 using DropboxLike.Domain.Data;
 using DropboxLike.Domain.Repositories.File;
@@ -66,7 +66,15 @@ builder.Services.AddAuthentication(x =>
         ClockSkew = TimeSpan.Zero
     };
 });
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Bearer", policy =>
+    {
+        policy.RequireClaim(ClaimTypes.Email);
+        policy.RequireClaim(ClaimTypes.NameIdentifier);
+    });
+    options.DefaultPolicy = options.GetPolicy("Bearer")!;
+});
 
 // 5. Add controllers.
 builder.Services.AddControllers();
@@ -84,7 +92,6 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthentication();
-app.UseCustomClaimValidation();
 app.UseAuthorization();
 
 app.MapControllers();
