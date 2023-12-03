18c886d4669ada5ddf808932b2237bb9b3c62ac6
Ruben te Wierik <ruben.wierik@vista.com>
2023-12-03 18:51:30 +0100

    Add parent to folder.

--- DropboxLike.Domain/Data/Entities/FolderEntity.cs
+++ DropboxLike.Domain/Data/Entities/FolderEntity.cs
@@ -12,6 +12,7 @@ public class FolderEntity
     public string FolderName { get; set; } = string.Empty;
     
     public UserEntity Owner { get; set; } = null!;
+    public FolderEntity? Parent { get; set; }
     public IEnumerable<FileEntity> Files { get; set; } = new List<FileEntity>();
     public IEnumerable<FolderEntity> Folders { get; set; } = new List<FolderEntity>();
 }
