75444175a44fbcaf6299eef7945d99ca18fb9fc8
Ruben te Wierik <ruben.wierik@vista.com>
2023-12-17 12:51:54 +0100

    Resolve merge conflicts.

--- DropboxLike.Domain/Data/Entities/FolderShareEntity.cs
+++ DropboxLike.Domain/Data/Entities/FolderShareEntity.cs
@@ -6,13 +6,5 @@ public class FolderShareEntity
     public string FolderId { get; set; }
 
     public UserEntity User { get; set; } = null!;
-<<<<<<< Updated upstream
     public virtual FolderEntity Folder { get; set; } = null!;
-=======
-<<<<<<< HEAD:DropboxLike.Domain/Data/Entities/ShareFolder.cs
-    public virtual FolderEntity Folder { get; set; } = null!;
-=======
-    public FolderEntity Folder { get; set; } = null!;
->>>>>>> 18c886d4669ada5ddf808932b2237bb9b3c62ac6:DropboxLike.Domain/Data/Entities/FolderShareEntity.cs
->>>>>>> Stashed changes
 }
